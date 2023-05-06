4027382bc2cf513d6ea5f3a24be9bd759061cdc6
legend479 <singhalprakhar7@gmail.com>
2023-05-06 19:53:01 +0530

    renamed:    HomePage/home.html -> DashboardPage/dashboard.html 	new file:   ImageResources/Logo.jpeg 	new file:   Navbar/navbar.css 	new file:   Navbar/navbar.html 	new file:   Navbar/navbar.js 	new file:   SignUpPage/signup.css 	new file:   SignUpPage/signup.html

---
+++ Navbar/navbar.js
@@ -0,0 +1,26 @@
+fetch('src/Navbar/navbar.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    var pageName = oldelem.dataset.page;
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    var items = newelem.getElementsByClassName('navbar')[0].getElementsByClassName('menu-item');
+    for (var i = 0 ; i < items.length; i++ ){
+        
+        if (items[i].innerHTML == pageName){
+            // console.log(item.innerHTML)
+
+            items[i].innerHTML = "<u>" + pageName + '</u>';
+        }
+        items[i].addEventListener('mouseover', (e)=>{
+            e.currentTarget.style.color = 'rgb(234, 122, 11)';
+            e.currentTarget.style.backgroundImage  = 'radial-gradient(ellipse at 50% 50%, rgba(236, 231, 185, 0.308), rgba(200, 186, 31, 0) 75%)';
+        });
+        items[i].addEventListener('mouseout', (e)=>{
+            e.currentTarget.style.color = 'var(--main-color)';
+            e.currentTarget.style.backgroundImage  = '';
+        });
+    }
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+})
